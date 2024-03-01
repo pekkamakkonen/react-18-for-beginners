e44362de2f1cf7df65900720a6c2d7080004f07b
AB4546 <AB4546@student.jamk.fi>
2024-03-01 14:26:58 +0200

    Passing Children (Alert component)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,13 @@
-import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
 
 function App() {
-  return <div><ListGroup /></div>
+  return (
+    <div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
