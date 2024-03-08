7f4154aca758455fc5201af92b37b69abaeba711
AB4546 <AB4546@student.jamk.fi>
2024-03-09 00:08:33 +0200

    Adding Icons

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,13 @@
 import ListGroup from './components/ListGroup';
 import './App.css';
+import { BsFillCalendarFill } from 'react-icons/bs';
 
 function App() {
   const items = ['New York', 'Los Angeles', 'San Francisco', 'Miami'];
 
   return (
     <div>
+      <BsFillCalendarFill color="red" size="40" />
       <ListGroup heading="Miami" items={items} onSelectItem={() => {}} />
     </div>
   );
