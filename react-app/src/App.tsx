3868d9b1a9fdadc399c3a1827da9015096407d8c
AB4546 <AB4546@student.jamk.fi>
2024-03-09 00:36:08 +0200

    Building a Like component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import ListGroup from './components/ListGroup';
 import './App.css';
 import { BsFillCalendarFill } from 'react-icons/bs';
+import Like from './components/Like';
 
 function App() {
   const items = ['New York', 'Los Angeles', 'San Francisco', 'Miami'];
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <BsFillCalendarFill color="red" size="40" />
+      <Like onClick={() => console.log('clicked')} />
       <ListGroup heading="Miami" items={items} onSelectItem={() => {}} />
     </div>
   );
